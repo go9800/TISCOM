0ec960cf456ab322c402e08a8ec499421f73df64
Achim <Achim@Spectre>
2018-07-28 22:09:23 +0200

    Test

--- analyzer/TMC0501Analyzer.java
+++ analyzer/TMC0501Analyzer.java
@@ -9,17 +9,17 @@ public class TMC0501Analyzer
 {
   int triggerValue = 0x01d8;
   int triggerAddr = 0x045f;
-  int SIZE = 1024;
+  int BUFFERSIZE = 1024;
 
-  // define GPIO pin numbers
-  final int PHI1 = 10;
-  final int IDLE = 12;
-  final int IRG = 13;
-  final int EXT = 14;
-  final int IO1 = 3;
-  final int IO2 = 4;
-  final int IO4 = 5;
-  final int IO8 = 6;
+  // define wiringPI pin numbers
+  final int PHI1 = 10; // GPIO 8
+  final int IDLE = 12; // GPIO 10
+  final int IRG = 13;  //GPIO 9
+  final int EXT = 14;  // GPIO 11
+  final int IO1 = 3;   // GPIO 22
+  final int IO2 = 4;   // GPIO 23
+  final int IO4 = 5;   // GPIO 24
+  final int IO8 = 6;   // GPIO 25
 
   // TMC0501 EXT bits
   final int EXT_PREG = 0b0000000000000001;
@@ -56,11 +56,13 @@ public class TMC0501Analyzer
   long t;
   long ioReg; // 16*4 bit value
   int irgReg, extReg;
-  int[] irgBuffer = new int[SIZE];
-  int[] extBuffer = new int[SIZE];
-  long[] ioBuffer = new long[SIZE];
-  int n, np, idle, addr, addr2;
-  int revTrigger;
+  int[] irgBuffer = new int[BUFFERSIZE];
+  int[] extBuffer = new int[BUFFERSIZE];
+  long[] ioBuffer = new long[BUFFERSIZE];
+  int[] addrBuffer = new int[BUFFERSIZE];
+  int writePos, readPos, numPhi, idle;
+  //int revTrigger;
+  //boolean loop = true;
 
   static TMC0501Dis disasm;
 
@@ -97,7 +99,7 @@ public class TMC0501Analyzer
 
   public TMC0501Analyzer()
   {
-    addr = triggerAddr;
+    int addr = triggerAddr, addr2;
 
     // setup wiring pi
     if (Gpio.wiringPiSetup() == -1) {
@@ -130,71 +132,80 @@ public class TMC0501Analyzer
     Gpio.pullUpDnControl(IO4, Gpio.PUD_OFF); // no pull-down resistor        
     Gpio.pinMode(IO8, Gpio.INPUT);
     Gpio.pullUpDnControl(IO8, Gpio.PUD_OFF); // no pull-down resistor  
-
+/*
     t = System.nanoTime();
-    np = 0;
-    revTrigger = (int)bitrev(triggerValue << 3, 16); // add S0 - S2 states then reverse bit order
+    numPhi = 0;
+    //revTrigger = (int)bitrev(triggerValue << 3, 16); // add S0 - S2 states then reverse bit order
 
-    for(n = 0; n < SIZE; ) {
-      idle = Gpio.digitalRead(IDLE); // get IDLE state
-      while(Gpio.digitalRead(PHI1) == 1); // wait for phi1 going low
-      //while(Gpio.digitalRead(PHI1) == 0); // wait for rising edge of phi1
+    while(loop) {
+      for(writePos = 0; writePos < SIZE; ) {
+        idle = Gpio.digitalRead(IDLE); // get IDLE state
+        while(Gpio.digitalRead(PHI1) == 1); // wait for phi1 going low
+        while(Gpio.digitalRead(PHI1) == 0); // wait for rising edge of phi1
 
-      // store registers after falling edge of IDLE
-      if(Gpio.digitalRead(IDLE) == 0) {
-        if(idle == 1) // IDLE was previously high
-          if(n > 0 || (irgReg & 0x1fff) == revTrigger) { // recording is running
-            irgBuffer[n] = irgReg;
-            extBuffer[n] = extReg;
-            ioBuffer[n] = ioReg;
-            n++;
-          }
+        // store registers after falling edge of IDLE
+        if(Gpio.digitalRead(IDLE) == 0) {
+          if(idle == 1) // IDLE was previously high
+            //if(writePos > 0 || (irgReg & 0x1fff) == revTrigger) { // recording is running
+            if(writePos > 0 || (irgReg == triggerValue)) { // recording is running
+              irgBuffer[writePos] = irgReg;
+              extBuffer[writePos] = extReg;
+              ioBuffer[writePos] = ioReg;
+              writePos++;
+            }
 
-        // clear shift register if phi1 and IDLE low
-        irgReg = extReg = 0;
-        ioReg = 0;
+          // clear shift register if phi1 and IDLE low
+          irgReg = extReg = 0;
+          ioReg = 0;
+        }
+
+        //while(Gpio.digitalRead(PHI1) == 0); // wait for rising edge of phi1
+
+        // shift IRG bit into IRG register (LSB first, bit reversal necessary!)
+        irgReg = irgReg >> 1 | (Gpio.digitalRead(IRG) == 1? 0x1000 : 0); // ignore S0 - S2 bits
+        // shift EXT bit into EXT register (LSB first)
+        extReg = extReg >> 1 | (Gpio.digitalRead(EXT) == 1? 0x8000 : 0);
+        // shift IO bits into IO register (LSB first)
+        ioReg = (((ioReg << 1 | Gpio.digitalRead(IO1)) << 1 | Gpio.digitalRead(IO2)) << 1 | Gpio.digitalRead(IO4)) << 1 | Gpio.digitalRead(IO8);
+        numPhi++;
       }
-
-      while(Gpio.digitalRead(PHI1) == 0); // wait for rising edge of phi1
-
-      // shift IRG bit into IRG register (LSB first, bit reversal necessary!)
-      irgReg = irgReg << 1 | Gpio.digitalRead(IRG);
-      // shift EXT bit into EXT register (LSB first)
-      extReg = extReg << 1 | Gpio.digitalRead(EXT);
-      // shift IO bits into IO register (LSB first)
-      ioReg = (((ioReg << 1 | Gpio.digitalRead(IO1)) << 1 | Gpio.digitalRead(IO2)) << 1 | Gpio.digitalRead(IO4)) << 1 | Gpio.digitalRead(IO8);
-      np++;
     }
 
     t = System.nanoTime() - t;
-    System.out.println("Sample rate: " + np * 1e9 / t);
+    System.out.println("Sample rate: " + numPhi * 1e9 / t);
     System.out.println("IRG trigger: " + longToHexString(triggerValue, 4));
+*/
+    Recorder recorder = new Recorder(irgBuffer, extBuffer, ioBuffer, BUFFERSIZE);
+    recorder.start(triggerValue, 1);
+    
+    for(readPos = 0; readPos < BUFFERSIZE; readPos++) {
+      //irgReg = (int)bitrev(irgBuffer[writePos], 16) >> 3; // eliminate S0 - S2 bits
+      //extReg = (int)bitrev(extBuffer[writePos], 16);
+      //ioReg = bitrev(ioBuffer[readPos], 64);
+      irgReg = irgBuffer[readPos];
+      extReg = extBuffer[readPos];
+      ioReg = ioBuffer[readPos];
 
-    for(n = 0; n < SIZE; n++) {
-      irgReg = (int)bitrev(irgBuffer[n], 16) >> 3; // eliminate S0 - S2 bits
-    extReg = (int)bitrev(extBuffer[n], 16);
-    ioReg = bitrev(ioBuffer[n], 64);
+      disasm.DisAssembleIt((short)addr, (short)irgReg);
+      System.out.println(longToDecString(readPos, 4) + ": " + longToHexString(irgReg,4) + "  " + longToBinString(extReg, 16) + "  " + longToHexString(ioReg, 16)  + " " + disasm.RomListing[addr].getMnemonicLine() );
 
-    disasm.DisAssembleIt((short)addr, (short)irgReg);
-    System.out.println(longToDecString(n, 4) + ": " + longToHexString(irgReg,4) + "  " + longToBinString(extReg, 16) + "  " + longToHexString(ioReg, 16)  + " " + disasm.RomListing[addr].getMnemonicLine() );
+      // get EXT bus of next sample
+      if(readPos + 1 < BUFFERSIZE)
+        extReg = (int)bitrev(extBuffer[readPos+1], 16);
 
-    // get EXT bus of next sample
-    if(n + 1 < SIZE)
-      extReg = (int)bitrev(extBuffer[n+1], 16);
+      // check EXT bus for HOLD bit
+      if((extReg & EXT_HOLD) != 0)
+        continue;  // don't increment address
 
-    // check EXT bus for HOLD bit
-    if((extReg & EXT_HOLD) != 0)
-      continue;  // don't increment address
+      // check for branch
+      addr2 = disasm.RomListing[addr].getAddr2();
+      addr++; // increment address in case of no branch
 
-    // check for branch
-    addr2 = disasm.RomListing[addr].getAddr2();
-    addr++; // increment address in case of no branch
-
-    if((irgReg & IRG_BRANCH) != 0) {
-      // branch only if condition bits in EXT and IRG match 
-      if(!((extReg & EXT_COND) != 0) ^ ((irgReg & IRG_COND) != 0))
-        addr = addr2; // set next address to branch destination
-    }
+      if((irgReg & IRG_BRANCH) != 0) {
+        // branch only if condition bits in EXT and IRG match 
+        if(!((extReg & EXT_COND) != 0) ^ ((irgReg & IRG_COND) != 0))
+          addr = addr2; // set next address to branch destination
+      }
     }
   }
 
